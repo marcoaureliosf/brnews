e0bf8e44f09120fc85efcca40a8f8a0c534d09cb
marcoaureliosf <aurelius.sf@gmail.com>
2022-04-13 12:48:57 -0300

    default settings

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+// import '../styles/home.module.scss'
+
+const Home: NextPage = () => {
+  return (
+    < >
+      <Head>
+        <title>Início | br.news</title>
+      </Head>
+    </>
+  )
+}
+
+export default Home
